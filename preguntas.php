e55601154c7d08cc7840cc8714c4271d2468fd91
Alvaro <alvarosantosmartin6@gmail.com>
2021-10-24 16:19:30 +0200

    simplemente creo la página para crear las preguntas, no es funcional

---
+++ preguntas.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/estilo.css">
+    <script src="js/validacion.js"></script>
+</head>
+
+<body>
+    <?php
+    session_start();
+    $_SESSION['volver'] = 'index';
+    ?>
+    <header class="row cabecera1">
+        <div class="s-col-12 m-col-12 l-col-12">
+            <h1>ATAKEBUNE</h1>
+        </div>
+    </header>
+    <div class="row">
+        <div class="s-col-4 m-col-4 l-col-4 offset-by-4">
+            <form action="" method="post" class="form1">
+                <div class="row">
+                    <textarea class="s-col-12 m-col-12 l-col-12" name="" id="" cols="30" rows="10" maxlength="400"></textarea>
+                </div>
+                <div class="row">
+                    <input class="s-col-10 m-col-10 l-col-10" type="text" name="" id="">
+                    <input class="s-col-10 m-col-10 l-col-10" type="text" name="" id="">
+                    <input class="s-col-10 m-col-10 l-col-10" type="text" name="" id="">
+                    <input class="s-col-10 m-col-10 l-col-10" type="text" name="" id="">
+                </div>
+                <div class="row">
+                    <div class="s-col-12 m-col-12 l-col-12">
+                        <input type="submit" value="Añadir pregunta" name="crear_pregunta">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
