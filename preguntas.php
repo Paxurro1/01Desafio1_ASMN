ad358bf3c47c1f6f997c3894c858bf78b7cefa72
Alvaro <alvarosantosmartin6@gmail.com>
2021-10-24 16:20:48 +0200

    quito una linea que me sobraba

--- preguntas.php
+++ preguntas.php
@@ -11,10 +11,6 @@
 </head>
 
 <body>
-    <?php
-    session_start();
-    $_SESSION['volver'] = 'index';
-    ?>
     <header class="row cabecera1">
         <div class="s-col-12 m-col-12 l-col-12">
             <h1>ATAKEBUNE</h1>
